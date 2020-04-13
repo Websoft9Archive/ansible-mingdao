bf4e2ac36dcc5c41cc1492d9e918096e78d65f19
laozhou0731 <62225175+laozhou0731@users.noreply.github.com>
2020-04-13 11:51:54 +0800

    Update sys.sh

--- roles/mingdao/templates/sys.sh
+++ roles/mingdao/templates/sys.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+if [[ -f /usr/local/mingdao/first ]];then rm -rf /data/mingdao/;fi 
+/bin/rm -f /usr/local/mingdao/first
 /bin/rm -f /usr/local/mingdao/service.pid
 /bin/bash /usr/local/mingdao/service.sh start

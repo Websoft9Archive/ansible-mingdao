fb57b4f46c82a3841df0d0d22da0b49a5addd77d
zengxc <zxc916684@163.com>
2021-05-18 10:29:54 +0800

    add init

--- roles/mingdao/templates/sys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [[ -f /usr/local/mingdao/first ]];then rm -rf /data/mingdao/;fi 
-/bin/rm -f /usr/local/mingdao/first
-/bin/rm -f /usr/local/mingdao/service.pid
-/bin/bash /usr/local/mingdao/service.sh start
